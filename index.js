bed17a66c03b067690c87c1d05c3e11106be5ae0
eastwo0d <roystonchua85@gmail.com>
2018-04-13 12:28:44 +0800

    ciy_assessment
    Assessment-Royston

---
+++ index.js
@@ -0,0 +1,96 @@
+const slider = document.querySelector('.items');
+let isDown = false;
+let startX;
+let scrollLeft;
+  
+slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+
+slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+
+slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return;  // stop the fn from running
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 3;
+    slider.scrollLeft = scrollLeft - walk;
+});
+
+var numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+
+
+function renderList(){
+    var result = '';
+    numbers.forEach((value, index)=>{
+        result = result + `<div class="item item${value}" id='item${value}'>${value}</div>`;
+    });
+    document.getElementById('items').innerHTML = result;
+}
+
+renderList();
+
+function doPush() {
+    numbers.push(numbers[numbers.length-1]+1);
+    var result = '';
+    numbers.forEach((value, index)=>{
+        result = result + `<div class="item item${value}" id='item${value}'>${value}</div>`;
+    });
+    document.getElementById('items').innerHTML = result;
+}
+
+function doPop() {
+    numbers.pop();
+    var result = '';
+    numbers.forEach((value, index)=>{
+        result = result + `<div class="item item${value}" id='item${value}'>${value}</div>`;
+    });
+    document.getElementById('items').innerHTML = result;
+}
+
+function doShift() {
+    numbers.shift();
+    var result = '';
+    numbers.forEach((value, index)=>{
+        result = result + `<div class="item item${value}" id='item${value}'>${value}</div>`;
+    });
+    document.getElementById('items').innerHTML = result;
+}
+
+function doUnShift() {
+    numbers.unshift(numbers[0]-1)
+    var result = '';
+    numbers.forEach((value, index)=>{
+        result = result + `<div class="item item${value}" id='item${value}'>${value}</div>`;
+    });
+    document.getElementById('items').innerHTML = result;
+}
+
+function doSplice () {
+    numbers.splice(3,1,4.5)
+    var result = '';
+    numbers.forEach((value, index)=>{
+        result = result + `<div class="item item${value}" id='item${value}'>${value}</div>`;
+    });
+    document.getElementById('items').innerHTML = result;
+}
+
+function doSort() {
+    numbers.sort(function(a, b){return b-a})
+    var result = '';
+    numbers.forEach((value, index)=>{
+        result = result + `<div class="item item${value}" id='item${value}'>${value}</div>`;
+    });
+    document.getElementById('items').innerHTML = result;
+}
